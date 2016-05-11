878a1ffe72ad8f90abc427833de1caa747ad3c9c
julijan <julijan.sribar@gmail.com>
2016-05-11 21:06:46 +0200

    Dodaj klasu SustavniPucač

---
+++ PotapanjeBrodova/SustavniPucač.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class SustavniPucač : IPucač
+    {
+        public SustavniPucač(IEnumerable<Polje> pogođena, Mreža mreža)
+        {
+            pogođenaPolja = new List<Polje>(pogođena);
+            this.mreža = mreža;
+        }
+
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Polje> pogođenaPolja;
+        Mreža mreža;
+    }
+}
